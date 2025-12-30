b9164e8d5e73421cf162dd7dea4e9e4f99814135
jmirabuenos-blip <Jmirabuenos@gbox.ncf.edu.ph>
2025-12-30 11:16:03 +0800

    live

--- book-app/my_django_app/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-# Register the Book model with custom admin options
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')  # Columns shown in admin list
-    search_fields = ('title', 'author')  # Searchable fields
-    # Removed list_filter
